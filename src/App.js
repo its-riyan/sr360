95f0c6c32af676bc95dc0636492332865f8f156b
its-riyan <81827910+its-riyan@users.noreply.github.com>
2021-12-01 22:28:09 +0700

    Add notifications

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-// import logo from "./react.png";
+import logo from "./react.png";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      <img src={logo} className="App-logo" alt="logo" />
       <div id="jsv-holder">
         <img
           id="jsv-image"
-          alt="your 360 images"
+          alt="Ranger Shoes"
           src="https://www.safetyranger.co.id/@static/kim/panorama/0001.jpg"
         />
       </div>
-      <div class="log">
+      <div className="log">
         <span id="log"></span>
         <span id="error-log"></span>
       </div>
--- src/index.js
+++ src/index.js
@@ -17,7 +17,18 @@ const safetyranger = new JavascriptViewer({
   imageUrlFormat: "000x.jpg",
   totalFrames: 49,
   reverse: true,
-  autoRotate: 1
+  autoRotate: 1,
+  defaultProgressBar: true,
+  speed: 90,
+  inertia: 12,
+  zoom: true,
+  notificationConfig: {
+    dragToRotate: {
+      showStartToRotateDefaultNotification: true,
+      mainColor: "rgba(0,0,0,0.20)",
+      textColor: "rgba(243,237,237,0.80)"
+    }
+  }
 });
 
 safetyranger
